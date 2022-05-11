a660cdac93ba6139042ae1f015e66c080892d8ff
hb-nasiruddin <hb.nasiruddinsaiyed@hiddenbrains.in>
2022-05-11 15:19:43 +0530

    Cypress Installation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-/**
- * @jest-environment jsdom
- */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/**
+ * @jest-environment jsdom
+ */
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
