568dda2bbf83f17b1e748e2872ee175d05ff4bb9
hb-nasiruddin <hb.nasiruddinsaiyed@hiddenbrains.in>
2022-05-12 15:48:16 +0530

    Fixing action

--- cypress/integration/app-spec.js
+++ cypress/integration/app-spec.js
@@ -3,7 +3,7 @@
 
 describe('CRA', () => {
   it('shows learn link', function () {
-    cy.visit('http://localhost:3000')
+    cy.visit(Cypress.env('host'))
     cy.get('.App-link').should('be.visible')
       .and('have.text', 'Learn React')
   })
